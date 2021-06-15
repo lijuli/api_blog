568b2dc3d7b8c81637e85640aaffef02940af409
eugeneulev <starmann27@yandex.ru>
2021-06-15 18:08:54 +0300

    First commit

--- users/views.py
+++ users/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404
+from rest_framework import filters, status, viewsets
+from rest_framework.decorators import api_view
+from rest_framework.permissions import (AllowAny, IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly)
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
 
-# Create your views here.
+from users.models import CustomUser
+from users.utils import get_random_code
+
+from .permissions import IsAdmin
+from .serializers import CustomUserSerializer, SafeCustomUserSerializer
+from .utils import send_mail_to_user
+from rest_framework_simplejwt.serializers import MyTokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = [IsAdmin]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['username', ]
+
+
+class APIUser(APIView):
+    def get(self, request):
+        if request.user.is_authenticated:
+            user = get_object_or_404(CustomUser, id=request.user.id)
+            serializer = CustomUserSerializer(user)
+            return Response(serializer.data)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+    def patch(self, request):
+        user = get_object_or_404(CustomUser, id=request.user.id)
+        if request.user == user:
+            serializer = SafeCustomUserSerializer(user, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+class RegisterView(APIView):
+    permission_classes = (AllowAny)
+
+    def post(self, request):
+        email = request.data.get('email')
+        check = CustomUser.objects.filter(email=email).exists()
+        if not check:
+            CustomUser.objects.create_user(email=email)
+        user = CustomUser.objects.filter(email=email)
+        confirmation_code = user.confirmation_code
+        send_mail_to_user(email, confirmation_code)
+        return Response({'email': email, 'confirmation_code': confirmation_code})
+
+
+# class TokenView(APIView):
+#     permission_classes = (AllowAny,)
+
+#     def post(self, request):
+#         user = get_object_or_404(CustomUser, email=request.data.get('email'))
+#         if user.confirmation_code != request.data.get('confirmation_code'):
+#             response = {'confirmation_code': 'Неправльный код'}
+#             return Response(response, status=status.HTTP_400_BAD_REQUEST)
+#         token = AccessToken.for_user(user)
+#         response = {'token': {token}}
+#         return Response(response, status=status.HTTP_200_OK)
