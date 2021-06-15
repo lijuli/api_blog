4d1c27707557700158e735677260df1dbfb414fd
eugeneulev <starmann27@yandex.ru>
2021-06-15 18:10:30 +0300

    First commit

--- api/urls.py
+++ api/urls.py
@@ -7,12 +7,8 @@ from .views import APIUser, CustomUserViewSet, RegisterView, TokenView, MyTokenO
 
 
 router_v1 = DefaultRouter()
-router_v1.register('users', CustomUserViewSet)
 
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
-    path('v1/users/me/', APIUser.as_view()),
-    path('v1/auth/email/', RegisterView.as_view(), name='registration'),
-    path('v1/auth/token/', MyTokenObtainPairView.as_view(), name='token'),
 ]
