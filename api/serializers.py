163aa87304fa33026c37348fa0a077869f051f1d
Julie <julia.mysyuk@gmail.com>
2021-06-16 11:22:09 -0400

    Added genres and categories.

--- api/serializers.py
+++ api/serializers.py
@@ -1,37 +1,58 @@
-from api.models.review import Review
+# from api.models.review import Review
 from rest_framework import serializers
 
 from api.models.title import Title
-from api.models.comment import Comment
+# from api.models.comment import Comment
+from api.models.category import Category
+from api.models.genre import Genre
 
 
 class TitleSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(max_length=500)
+    category = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Category.objects.all(),
+    )
+    genre = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Genre.objects.all(),
+    )
 
     class Meta:
         fields = '__all__'
         model = Title
 
 
-class ReviewSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True,
-        slug_field='username'
-    )
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
 
+
+class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
-        read_only_fields = ('title',)
-        model = Review
+        model = Genre
 
 
-class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True,
-        slug_field='username'
-    )
-
-    class Meta:
-        fields = '__all__'
-        read_only_fields = ('review',)
-        model = Comment
+# class ReviewSerializer(serializers.ModelSerializer):
+#     author = serializers.SlugRelatedField(
+#         read_only=True,
+#         slug_field='username'
+#     )
+#
+#     class Meta:
+#         fields = '__all__'
+#         read_only_fields = ('title',)
+#         model = Review
+#
+#
+# class CommentSerializer(serializers.ModelSerializer):
+#     author = serializers.SlugRelatedField(
+#         read_only=True,
+#         slug_field='username'
+#     )
+#
+#     class Meta:
+#         fields = '__all__'
+#         read_only_fields = ('review',)
+#         model = Comment
--- api/urls.py
+++ api/urls.py
@@ -2,14 +2,18 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.views import TitleViewSet
-from api.views import ReviewViewSet
+# from api.views import ReviewViewSet
+from api.views import CategoryViewSet
+from api.views import GenreViewSet
 
 router_v1 = DefaultRouter()
-router_v1.register('v1/titles', TitleViewSet, basename='title_api')
-router_v1.register(r'v1/titles/(?P<title_id>[0-9]+)/reviews',
-                   ReviewViewSet,
-                   basename='reviews_api')
+router_v1.register('titles', TitleViewSet, basename='title_api')
+# router_v1.register(r'v1/titles/(?P<title_id>[0-9]+)/reviews',
+#                    ReviewViewSet,
+#                    basename='review_api')
+router_v1.register(r'categories', CategoryViewSet, basename='category_api')
+router_v1.register(r'genres', GenreViewSet, basename='genre_api')
 
 urlpatterns = [
-    path('', include(router_v1.urls)),
+    path('v1/', include(router_v1.urls)),
 ]
