69c0818a779b941501c59ea84471620117ba4afc
eugeneulev <starmann27@yandex.ru>
2021-06-19 19:18:31 +0300

    First commit

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -17,13 +17,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'drf_yasg',
     'rest_framework',
     'django_filters',
     'rest_framework_simplejwt',
     'users',
     'api',
-
 ]
 
 MIDDLEWARE = [
